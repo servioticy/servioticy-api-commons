89925b671e1b5c12b52114fc5953d73e67d5b8e5
jlupox <jlupox@gmail.com>
2014-04-23 17:13:13 +0200

    Fix responseStreams loop

--- src/main/java/com/servioticy/api/commons/data/SO.java
+++ src/main/java/com/servioticy/api/commons/data/SO.java
@@ -320,9 +320,10 @@ public class SO {
     try {
       Map<String, JsonNode> mstreams = mapper.readValue(streams.traverse(), new TypeReference<Map<String, JsonNode>>() {});
       ArrayList<JsonNode> astreams = new ArrayList<JsonNode>();
-      JsonNode s = mapper.createObjectNode();
+      JsonNode s;
 
       for (Map.Entry<String, JsonNode> stream : mstreams.entrySet()) {
+    	s = mapper.createObjectNode();
         ((ObjectNode)s).put("name", stream.getKey());
         ((ObjectNode)s).putAll((ObjectNode)stream.getValue());
         ((ObjectNode)s).remove("data");
