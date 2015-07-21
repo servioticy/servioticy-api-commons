9ce03b883f61849eea6cfc08c7ad262abd6f059d
jlupox <jlupox@gmail.com>
2015-07-21 12:26:00 +0200

    upgrade pdp to 0.2.0 and added deleteSO from IDM

--- src/main/java/com/servioticy/api/commons/security/IDM.java
+++ src/main/java/com/servioticy/api/commons/security/IDM.java
@@ -81,6 +81,36 @@ public class IDM {
 		return null;
 	}
 	
+	public static void DeleteSO(String auth_token,
+	  							String soId,
+								boolean requires_token,
+								boolean data_provenance_collection,
+								boolean payment,
+								String host,
+								int port) {
+
+		IDMCommunicator com = new IDMCommunicator(Config.idm_user, Config.idm_password, host, port);
+		
+        String access_token_user = "Bearer " + auth_token;
+		try{
+		    //add the bearer key word.
+            com.deleteSO("http", host, port, soId, access_token_user);
+        }
+        catch(PDPServioticyException e)
+        {
+			int statusCode = e.getStatus();
+            //here code could be 401 or 403 or 404 and there is always a user message...
+			if (statusCode == 401)
+				throw new ServIoTWebApplicationException(Response.Status.UNAUTHORIZED, e.getMessage());
+			if (statusCode == 403)
+				throw new ServIoTWebApplicationException(Response.Status.FORBIDDEN, e.getMessage());
+//			if (statusCode == 404) POSSIBLE LOGGING
+            //TODO handle:
+//            System.out.println("User message: "+e.getMessage()+" Status: "+e.getStatus()+" Logging info: "+e.getLogInfo());
+        }
+
+	}
+	
 	public static String random_auth_token(String accessToken) {
         ObjectMapper mapper = new ObjectMapper();
 
